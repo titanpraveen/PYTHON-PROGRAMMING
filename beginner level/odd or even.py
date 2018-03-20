2e1005024919bf868d8c455e4c5552af7c770a5d
titanpraveen <37335992+titanpraveen@users.noreply.github.com>
2018-03-20 10:23:11 +0530

    Update odd or even.py

--- beginner level/odd or even.py
+++ beginner level/odd or even.py
@@ -1,5 +1,5 @@
-    a=int(input("ENTER THE NUMBER "))
-    if(a%2==0):
-    	print("IT IS EVEN")
-    else:
-    	print("IT IS ODD")
+a=int(input("ENTER THE NUMBER "))
+if(a%2==0):
+	print("IT IS EVEN")
+else:
+            print("IT IS ODD")
