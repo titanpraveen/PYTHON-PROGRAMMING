5a381e5937a66c53cefeb49f5192fa1570cb4802
titanpraveen <37335992+titanpraveen@users.noreply.github.com>
2018-03-20 11:59:06 +0530

    Create LARGEST OF THREE .py

---
+++ beginner level/LARGEST OF THREE .py
@@ -0,0 +1,10 @@
+a = int(input("enter 1 number"))
+b = int(input("enter 2 number"))
+c = int(input("enter 3 number"))
+if(a>b and a>c):
+  print(" 1 IS GREATER ")
+elif(b>c):
+  print("2 IS GREATER")
+else:
+  print(" 3 IS GREATER")
+    
