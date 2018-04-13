295b5af6e7276a87fe8b12524c93955238aad898
titanpraveen <37335992+titanpraveen@users.noreply.github.com>
2018-04-13 18:36:46 +0530

    Create TWO NUMBER AND ADDITION OF GIVEN NUMBERS.py

---
+++ beginner level/TWO NUMBER AND ADDITION OF GIVEN NUMBERS.py
@@ -0,0 +1,14 @@
+a=int(input("ENTER THE SEQUENCE NUMBER"))
+b=int(input("ENTER THE SUM NUMBER"))
+b=b+1
+d=0
+c=1
+for i in range(c,a+1):
+  
+  b=b-1
+  if(b!=0):
+   d=d+i 
+   print(d)
+  if(b==0):
+   exit()
+
